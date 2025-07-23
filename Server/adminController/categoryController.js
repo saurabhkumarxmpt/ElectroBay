bb0266ef23b8be1b9e872bc7955c5043b362251a
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-07-23 17:21:09 +0530

    make some backend

---
+++ Server/adminController/categoryController.js
@@ -0,0 +1,2 @@
+const Category = require('../models/categorySchema');
+
---
+++ Server/models/categorySchema.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+const categorySchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+});
+
+const Category=mongoose.model('Category',categorySchema);
+module.exports=Category;
