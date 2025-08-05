b9ab28749ed4bddf5b6bc99a7c7e20bea9f87d5f
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-08-05 21:21:50 +0530

    make some backend

--- Server/routes/productRoutes.js
+++ Server/routes/productRoutes.js
@@ -1,8 +1,8 @@
 const express=require("express");
 const router=express.Router();
 const upload=require('../utils/multer');
-const{createProduct}=require('../controllers/productController');
+const{createProduct,fetchAllProducts}=require('../controllers/productController');
 
 router.post('/', upload.array('images',5),createProduct); //create the new product
-
+router.get('/allProducts',fetchAllProducts);
 module.exports=router;
