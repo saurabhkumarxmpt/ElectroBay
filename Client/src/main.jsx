e24ff2622691d80c358141834102e57424a97c23
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-08-13 21:47:24 +0530

    make the product slice

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import AppRoutes from './routes/AppRoutes'
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
     <AppRoutes />
+    </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
